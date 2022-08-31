e86264a5abe359b7717e23781806ff01c447f17d
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-08-31 15:04:56 +0200

    server.js

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,20 @@
 # FROM scottyhardy/docker-wine:latest
-FROM stefanscherer/node-windows:latest
+FROM node:lts-buster
 COPY . .
+# RUN apt-get update \
+#   && apt-get install -y --no-install-recommends apt-transport-https
+# RUN dpkg --add-architecture i386 \
+#   && wget -nc https://dl.winehq.org/wine-builds/winehq.key \
+#   && apt-key add winehq.key \
+#   && echo "deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./" >> /etc/apt/sources.list
+# RUN apt-get update \
+#   && apt-get install -y winehq-stable
+# Wine stuff 
+RUN dpkg add-architecture i386 && apt-get update && wget -qO- https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
+RUN apt install software-properties-common &&  apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+RUN apt update && apt install --install-recommends winehq-stable
+RUN wine --version
+# Install modules and start the server 
+
 RUN npm install 
 CMD npm start 
--- server.js
+++ server.js
@@ -9,21 +9,23 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-var exec = require('child_process').exec;
+  var exec = require('child_process').exec;
 
-exec('wsynth.exe', ['-model_type', 'dn', '-out_type', 'dot', '-algo', 'agaf_then_acyclic_preferences', '-agaf', 'states', '-mono', '-dynamic', '-reachability_analysis', './test.smv'], function(err, data) {
-if (err) {
-console.log("ERRORE: " + err);
-res.json('{ Error: ' + err + '}');
-}
-else {
-console.log("OK: " + data.toString());
-res.json('{ success: ' + data + '}');
-}
+  exec('wsynth.exe', ['-model_type', 'dn', '-out_type', 'dot', '-algo', 'agaf_then_acyclic_preferences', '-agaf', 'states', '-mono', '-dynamic', '-reachability_analysis', './test.smv'], function(err, data) {
+    if (err) {
+      console.log("ERRORE: " + err);
+      res.json('{ Error: ' + err + '}');
+    }
+    else {
+      console.log("OK: " + data.toString());
+      res.json('{ success: ' + data + '}');
+    }
+  });
+
 });
 
-});
-
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 // app.post('/', (req, res) => {
 
 
@@ -39,11 +41,6 @@ res.json('{ success: ' + data + '}');
 // else
 // console.log("OK: " + data.toString());
 // });
+// res.send('Hello World');
+// });
 
-
-
-res.send('Hello World');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
