451cd2c202f84dd61d306fbb4c71257c10f6f981
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-09-01 15:10:23 +0200

    Added the option to add your file

--- routes/converter.js
+++ routes/converter.js
@@ -2,16 +2,25 @@ const express = require("express")
 const exec = require('child_process').exec;
 
 converterRoutes = express.Router()
+// tr -d <file> per fare un single liner 
 
-// Invia il grafo transformato 
-converterRoutes.get('/convert', (req, res) => {
-exec(
-  'wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv',
- function(error, stdout, stderr) {
+// POST: riceve in json il file, lo pipa e poi ne ricava il contenuto  
+converterRoutes.post('/convert', (req, res) => {
+  exec("echo " + req.body.data + "> test.smv",
+    function(error, stdout, stderr) {
       if (error != null) {
-        console.log("ERRORE: " + error);
-        return res.status(500).json({ Error: error });
-    } else {
+        exec(
+          'wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv',
+          function(error, stdout, stderr) {
+            if (error != null) {
+              console.log("ERRORE: " + error);
+              return res.status(500).json({ Error: error });
+            } else {
+              console.log("OK: " + stdout);
+              return res.status(200).json({ Data: stdout });
+            }
+          })
+      } else {
         console.log("OK: " + stdout);
         return res.status(200).json({ Data: stdout });
       }
