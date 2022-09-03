5d0759b2b60ff61265c8291f3c63b6e21a2900f8
Kativen <mangrella.andrea@gmail.com>
2022-09-03 04:44:08 -0400

    fixed typo and added pipe of streams with cat

--- routes/converter.js
+++ routes/converter.js
@@ -6,11 +6,13 @@ converterRoutes = express.Router()
 
 // POST: riceve in json il file, lo pipa e poi ne ricava il contenuto  
 converterRoutes.post('/convert', (req, res) => {
-  exec("echo " + req.body.data + "> test.smv",
+  exec("echo \"" + req.body.data+ "\" > test.smv",
     function(error, stdout, stderr) {
       if (error != null) {
-        exec(
-          'wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv',
+         console.log("ERRORE: " + error);
+         return res.status(500).json({ Error: error });
+      } else {
+	 exec('wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv &> output.dot && cat output.dot',
           function(error, stdout, stderr) {
             if (error != null) {
               console.log("ERRORE: " + error);
@@ -18,11 +20,9 @@ converterRoutes.post('/convert', (req, res) => {
             } else {
               console.log("OK: " + stdout);
               return res.status(200).json({ Data: stdout });
+	      //return res.status(200).attachment("output.dot");
             }
           })
-      } else {
-        console.log("OK: " + stdout);
-        return res.status(200).json({ Data: stdout });
       }
     });
 });
