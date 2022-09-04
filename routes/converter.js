f52b9ebf8251606040d62a85403ee32bbde3b90f
Kativen <mangrella.andrea@gmail.com>
2022-09-04 07:17:39 -0400

    fixed pipe and tested locally

--- routes/converter.js
+++ routes/converter.js
@@ -12,7 +12,7 @@ converterRoutes.post('/convert', (req, res) => {
          console.log("ERRORE: " + error);
          return res.status(500).json({ Error: error });
       } else {
-	 exec('wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv &> output.dot && cat output.dot',
+	 exec('wine wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis ./test.smv > output.dot 2>&1  && cat output.dot',
           function(error, stdout, stderr) {
             if (error != null) {
               console.log("ERRORE: " + error);
